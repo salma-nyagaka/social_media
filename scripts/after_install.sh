1dd08a60a56d10322c9843e502fd8adce0af3225
salma-nyagaka <salmanyagaka@gmail.com>
2024-06-04 21:32:45 +0300

    add codedeploy config

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -2,6 +2,11 @@
 # This script runs after the Install phase
 # e.g., cleaning up, setting permissions
 #!/bin/bash
+echo "Before install script running..."
+# Remove existing Dockerfile if it exists
+if [ -f /var/www/twiga/social_media/social_media_project/Dockerfile ]; then
+    rm /var/www/twiga/social_media/social_media_project/Dockerfile
+fi
 
 # Navigate to app directory
 cd /var/www/twiga/social_media/social_media_project
--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -3,11 +3,7 @@
 # e.g., stopping services or backing up data
 #!/bin/bash
 #!/bin/bash
-echo "Before install script running..."
-# Remove existing Dockerfile if it exists
-if [ -f /var/www/twiga/social_media/social_media_project/Dockerfile ]; then
-    rm /var/www/twiga/social_media/social_media_project/Dockerfile
-fi
+
 # Any other necessary cleanup or preparation commands can be added here
 docker stop $(docker ps -q) || true
 docker rm $(docker ps -a -q) || true
