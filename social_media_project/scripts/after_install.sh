9d3efcbb87d58364985a1406084495dc9dcf8816
salma-nyagaka <salmanyagaka@gmail.com>
2024-06-04 21:00:26 +0300

    add codedeploy config

---
+++ social_media_project/scripts/after_install.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# This script runs after the Install phase
+# e.g., cleaning up, setting permissions
---
+++ social_media_project/scripts/before_install.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# This script runs before the Install phase
+# e.g., stopping services or backing up data
