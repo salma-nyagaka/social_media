106a81f545c2bf75bdfd63895a13c409823dd783
salma-nyagaka <salmanyagaka@gmail.com>
2024-06-05 08:01:47 +0300

    add codedeploy config

--- social_media_project/celery.py
+++ social_media_project/celery.py
@@ -4,7 +4,7 @@ from celery import Celery
 from django.conf import settings
 
 # set the default Django settings module for the 'celery' program.
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "social_media_project.apps.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "social_media_project.settings")
 
 app = Celery("social_media_project")
 
