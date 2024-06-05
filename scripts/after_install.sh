0443f9f40c083d1e7b1a4f0c710af1e87b24e1f3
salma-nyagaka <salmanyagaka@gmail.com>
2024-06-05 08:38:20 +0300

    add codedeploy config

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -7,6 +7,9 @@ cd /var/www/twiga/social_media/social_media_project
 sudo docker stop myapp || true
 sudo docker rm myapp || true
 
+sudo cp -r /var/www/twiga/social_media/social_media_project /var/www/twiga/social_media/deployed_app
+
+
 # Build and run the new container
 sudo docker build -t myapp .
 sudo docker run -d -p 8080:8000 --name myapp myapp
