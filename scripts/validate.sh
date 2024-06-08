2e26c206315610572e6c8b7a60a6efea80c75951
salma-nyagaka <salmanyagaka@gmail.com>
2024-06-08 15:54:13 +0300

    add deploy configs

--- scripts/validate.sh
+++ scripts/validate.sh
@@ -1,6 +1,9 @@
-#!/bin/bash
-# Navigate to project directory
-cd /opt/twiga/social_media
+# #!/bin/bash
+# # Navigate to project directory
+# cd /opt/twiga/social_media
 
-# Start Docker containers
-docker-compose up -d
+# # Start Docker containers
+# docker-compose up -d
+#!/bin/bash
+# Check if the application is running
+curl -f http://localhost:8000 || exit 1
