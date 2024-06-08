6c772bc8fa2130c30b2d29ff6a78ec4517ba3ce9
salma-nyagaka <salmanyagaka@gmail.com>
2024-06-08 17:02:49 +0300

    add deploy configs

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -61,24 +61,24 @@ sudo apt-get update
 sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common
 
 # Add Docker’s official GPG key
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+# curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 
 # Add Docker’s stable repository
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+# sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 
 # Update package list
 sudo apt-get update
 
 # Install Docker
-sudo apt-get install -y docker-ce docker-ce-cli containerd.io
+# sudo apt-get install -y docker-ce docker-ce-cli containerd.io
 
 # Start and enable Docker
 sudo systemctl start docker
 sudo systemctl enable docker
 
 # Install Docker Compose
-sudo curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
+# sudo curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+# sudo chmod +x /usr/local/bin/docker-compose
 
 # Remove the existing directory or file
 sudo rm -rf /opt/twiga/social_media/
