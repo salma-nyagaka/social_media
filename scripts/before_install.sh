0bc385d438f28370684fd34d236157de2af69c96
salma-nyagaka <salmanyagaka@gmail.com>
2024-06-08 16:03:32 +0300

    add deploy configs

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -39,15 +39,15 @@ sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubun
 sudo apt-get update
 
 # Install Docker
-# sudo apt-get install -y docker-ce docker-ce-cli containerd.io
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io
 
 # Start and enable Docker
 sudo systemctl start docker
 sudo systemctl enable docker
 
 # Install Docker Compose
-# sudo curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-# sudo chmod +x /usr/local/bin/docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
 
 # Remove the existing directory or file
 sudo rm -rf /opt/twiga/social_media/
