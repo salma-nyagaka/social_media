f202182a271c32d65d54473453c9da33db544a50
salma-nyagaka <salmanyagak@gmail.com>
2024-06-10 12:53:19 -0400

    add db migrations

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 # Navigate to project directory
 cd /opt/twiga/social_media
+docker-compose run web python manage.py makemigrations
+docker-compose run web python manage.py migrate
 
 # Start Docker containers
-docker-compose up -d --build
+docker-compose up -d
