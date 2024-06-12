d8c840d5c2dcbc35639b8bca6b927c08e6b81d6c
salma-nyagaka <salmanyagak@gmail.com>
2024-06-12 06:37:59 -0400

    delete migration lines

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # Navigate to project directory
 cd /opt/twiga/social_media
-docker-compose run web python manage.py makemigrations
-docker-compose run web python manage.py migrate
+# docker-compose run web python manage.py makemigrations
+# docker-compose run web python manage.py migrate
 
 # Start Docker containers without rebuilding every time
 docker-compose up -d
