37b81d665965e9e864e8f58b29943ae28f5dd11b
salma-nyagaka <salmanyagaka@gmail.com>
2024-06-13 14:40:39 +0300

    update scripts and docker comose

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,9 +1,24 @@
 #!/bin/bash
-
 set -e
 
 # Navigate to the project directory
 cd /opt/twiga/social_media
 
-# Start Docker containers
-docker-compose up -d
+# Check if the .env file exists, create if it doesn't
+if [ ! -f .env ]; then
+  echo "Creating .env file..."
+  echo "SECRET_KEY=${SECRET_KEY}" > .env
+  echo "DATABASE_URL=${DATABASE_URL}" >> .env
+  echo "EMAIL_HOST_PASSWORD=${EMAIL_HOST_PASSWORD}" >> .env
+  echo "DEFAULT_FROM_EMAIL=${DEFAULT_FROM_EMAIL}" >> .env
+  echo "DOMAIN_NAME=${DOMAIN_NAME}" >> .env
+fi
+
+# Pull the latest Docker images
+docker-compose pull
+
+# Remove any existing containers
+docker-compose down
+
+# Start Docker containers without rebuilding every time
+docker-compose up -d --build
--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -4,11 +4,17 @@ set -e
 # Navigate to the project directory
 cd /opt/twiga/social_media
 
-# Pull the latest Docker images
-docker-compose pull
 
-# Remove any existing containers
-docker-compose down
 
-# Start Docker containers without rebuilding every time
-docker-compose up -d
+docker-compose up -d --build
+
+# # Pull the latest Docker images
+# docker-compose pull
+
+# # Remove any existing containers
+# docker-compose down
+
+# # Start Docker containers without rebuilding every time
+# docker build -t  limsapi/social_media:latest .
+
+# docker-compose up -d
