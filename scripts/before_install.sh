0547a8f6d0f0df6ebe3a4a23df44146ce79c6c6b
salma-nyagaka <salmanyagaka@gmail.com>
2024-06-13 23:56:21 +0300

    update before install

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -5,17 +5,6 @@ DEPLOYMENT_GROUP=$(aws deploy list-deployment-groups --application-name credrail
 export DEPLOYMENT_GROUP
 
 
-# Clean CodeDeploy-agent files for a fresh install
-sudo rm -rf /home/ubuntu/install
-
-# Install CodeDeploy agent
-sudo apt-get -y update
-sudo apt-get -y install ruby wget
-cd /home/ubuntu
-wget https://aws-codedeploy-us-east-1.s3.amazonaws.com/latest/install
-sudo chmod +x ./install 
-sudo ./install auto
-
 # Update OS & install Python3
 sudo apt-get update
 # sudo apt-get install -y python3 python3-dev python3-pip python3-venv
