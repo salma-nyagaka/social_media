6098e06e452ba410ac7ff3fbeb92c6cc14798ac7
salma-nyagaka <salmanyagak@gmail.com>
2024-06-14 10:26:24 -0400

    update deployment process

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-sudo apt-get remove -y docker docker-engine docker.io containerd runc
-
 # Clean up unnecessary files to free up space
 sudo apt-get clean
 sudo rm -rf /var/lib/apt/lists/*
@@ -9,21 +7,7 @@ sudo rm -rf /var/log/*
 sudo rm -rf /tmp/*
 sudo rm -rf /var/tmp/*
 
-# Install dependencies
-sudo apt-get update
-sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common
-
-# Add Docker’s official GPG key
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-
-# Add Docker’s stable repository
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-
-# Update package list
-sudo apt-get update
-
-# Install Docker
-sudo apt-get install -y docker-ce docker-ce-cli containerd.io
+sudo docker system prune -a
 
 # Start and enable Docker
 sudo systemctl start docker
--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 # Navigate to project directory
 cd /opt/twiga/social_media
-# docker-compose run web python manage.py makemigrations
-# docker-compose run web python manage.py migrate
 
-# Start Docker containers without rebuilding every time
-docker-compose up -d
+docker-compose pull
+docker-compose up -d --build
