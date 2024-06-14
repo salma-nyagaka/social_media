db16acf76912a095149c15cd357fc45cb7e707ec
salma-nyagaka <salmanyagaka@gmail.com>
2024-06-14 20:46:32 +0300

    reconfigure

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -4,20 +4,20 @@
 cd /opt/twiga/social_media
 
 # Stop and remove the existing container (if running)
-docker stop social_media_project || true
-docker stop social_media_app || true
-docker rm social_media_project || true
-docker rm social_media_app || true
+sudo docker stop social_media_project || true
+sudo docker stop social_media_app || true
+sudo docker rm social_media_project || true
+sudo docker rm social_media_app || true
 
 # Remove all Docker volumes
-docker volume rm $(docker volume ls -q) || true
+sudo docker volume rm $(docker volume ls -q) || true
 
 # Copy the .env file to the project directory
 cp /opt/.env /opt/twiga/social_media/.env
 
 # Bring down any existing containers
-docker-compose down
+sudo docker-compose down
 
 # Remove any existing containers
-docker stop social_media_app || true
-docker rm social_media_app || true
+sudo docker stop social_media_app || true
+sudo docker rm social_media_app || true
