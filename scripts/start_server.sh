0a7c77142a60ab42d307c1f9a33bd2ad321ca649
salma-nyagaka <salmanyagak@gmail.com>
2024-06-14 12:20:24 -0400

    update deployment process

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
-# Navigate to project directory
+
+# Navigate to the project directory
 cd /opt/twiga/social_media
+
+# Pull the latest Docker image
 docker pull limsapi/social_media:latest
+
+# Copy the .env file to the project directory
 cp /opt/.env /opt/twiga/social_media/.env
-docker run -d --name social_media_app limsapi/social_media:latest
+
+# Stop the existing container if it is running
+docker stop social_media_app || true
+
+# Remove the existing container if it exists
+docker rm social_media_app || true
+
+# Run the new container
+docker run -d -p 8000:8000 --env-file /opt/.env --name social_media_app limsapi/social_media:latest
